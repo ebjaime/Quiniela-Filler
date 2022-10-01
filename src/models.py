99b5c5aeb3914cefd38de7cd6737e1d28297fb00
ebjaime <j.enriquezballe@gmail.com>
2022-10-01 21:35:25 +0200

    SVM+GBC+LSTM

--- src/models.py
+++ src/models.py
@@ -6,13 +6,15 @@ import requests
 import os
 
 from sklearn.preprocessing import RobustScaler, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
+from sklearn.svm import SVC
 
 import keras
 import keras.models
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, LSTM, SpatialDropout1D
 from keras.utils import plot_model, to_categorical
 
 import tensorflow as tf
@@ -33,23 +35,8 @@ class Quiniela:
         if self.model2 is None:
             self.model2.train(batch_size=8)
 
-    def predict_quiniela_pct(self, pleno_al_15=1, *args):
-        q1 = self.model1.predict_quiniela_pct(*args)
-        q2 = self.model2.predict_quiniela_pct(*args)
-        if pleno_al_15 == 1:
-            q = pd.concat([q1.iloc[:-1, :], q2], ignore_index=True)
-            q = q.append(q1.iloc[-1, :], ignore_index=True)
-        else:
-            q = pd.concat([q1, q2.iloc[:-1, :]], ignore_index=True)
-            q = q.append(q2.iloc[-1, :], ignore_index=True)
-        q.index = range(1, 16)
-        # Change column name from HDA to 1X2
-        q[["1", "X", "2"]] = q[["H", "D", "A"]]
-        q.drop(["H", "D", "A"], axis=1, inplace=True)
-        return q
-
-    def predict_quiniela(self, pleno_al_15=1, *args):
-        pct = self.predict_quiniela_pct(pleno_al_15=1, *args)
+    def predict_quiniela(self, pleno_al_15=1, **kwargs):
+        pct = self.predict_quiniela_pct(pleno_al_15=1, **kwargs)
         dobles = self.predict_dobles(pct)
         # Choose best option for each fixture
         pct["1X2"] = pct[["1", "X", "2"]].idxmax(axis=1)
@@ -58,10 +45,25 @@ class Quiniela:
         pct["Dobles"] = dobles
         return pct
 
+    def predict_quiniela_pct(self, pleno_al_15=1, **kwargs):
+        q1 = self.model1.predict_quiniela_pct(**kwargs)
+        q2 = self.model2.predict_quiniela_pct(**kwargs)
+        if pleno_al_15 == 1:
+            q = pd.concat([q1.iloc[:-1, :], q2], ignore_index=True)
+            q = q.append(q1.iloc[-1, :], ignore_index=True)
+        else:
+            q = pd.concat([q1, q2.iloc[:-1, :]], ignore_index=True)
+            q = q.append(q2.iloc[-1, :], ignore_index=True)
+        q.index = range(1, q.shape[0] + 1)
+        # Change column name from HDA to 1X2
+        q[["1", "X", "2"]] = q[["H", "D", "A"]]
+        q.drop(["H", "D", "A"], axis=1, inplace=True)
+        return q
+
     def predict_dobles(self, pct):
         preds_pct = pct.copy()
         col = preds_pct[["1", "X", "2"]].idxmax(axis=1)
-        for j in range(1, 16):
+        for j in range(1, pct.shape[0] + 1):
             preds_pct.loc[j, col[j]] = 0
         dobles = pd.Series(np.repeat("-", 15), index=range(1, 16))
         for i in range(self.dobles):
@@ -87,22 +89,32 @@ class QuinielaFillerBase:
     def train(self):
         pass
 
-    def predict(self, xt):
-        pass
+    def predict(self, xt, **kwargs):
+        categorical = ["home_team", "away_team", "time"]
+        unnecessary = ["total_valA", "total_valH"]
 
-    def pretty_predict(self, xt, *args):
-        preds = self.predict(xt, *args)
+        df_num = xt.drop(columns=categorical + unnecessary)
+        df_num[["AvgH", "AvgA", "AvgD"]] = 1 / df_num[
+            ["full_time_result.1", "full_time_result.2", "full_time_result.X"]]
+        df_num.drop(["full_time_result.1", "full_time_result.2", "full_time_result.X"], axis=1, inplace=True)
+        xt_scaled = self.rob_scaler.transform(df_num[self.columns])  # Apply transformer + Resort columns
+
+        preds = self.model.predict_proba(xt_scaled, **kwargs)
+        return preds
+
+    def pretty_predict(self, xt, **kwargs):
+        preds = self.predict(xt, **kwargs)
         preds_df = pd.DataFrame(preds, columns=self.encoder.classes_)
         return pd.concat([xt[["time", "home_team", "away_team"]], preds_df[["H", "D", "A"]]], axis=1)
 
-    def predict_quiniela_pct(self, pretty=True, *args):
+    def predict_quiniela_pct(self, pretty=True, **kwargs):
         live_data = prepare_live_data(liga=self.liga)
         quiniela = prepare_quiniela()
         quiniela_live = pd.merge(live_data, quiniela, how="inner")
         if pretty:
-            return self.pretty_predict(quiniela_live, *args)
+            return self.pretty_predict(quiniela_live, **kwargs)
         else:
-            return self.predict(quiniela_live, *args)
+            return self.predict(quiniela_live, **kwargs)
 
     def evaluate(self, xt, yt):
         pass
@@ -157,6 +169,7 @@ class QuinielaFillerBase:
         # TODO: [3] Current data TransferMrkt
 
     def preprocessing(self):
+        # TODO: Mix all samples between them
         categorical = ["HomeTeam", "AwayTeam", "Date", "FTR", "season"]
         unnecessary = ["total_valA", "total_valH"]
 
@@ -165,8 +178,8 @@ class QuinielaFillerBase:
         df_Y = self.raw_data["FTR"]
 
         self.rob_scaler = RobustScaler()
-        self.rob_scaler.fit(df_num)
-        df_rob = self.rob_scaler.transform(df_num)
+        df_rob = self.rob_scaler.fit_transform(df_num)
+        self.columns = df_num.columns
 
         self.encoder = LabelEncoder()
         self.encoder.fit(df_Y)
@@ -177,26 +190,22 @@ class QuinielaFillerBase:
 
 
 class QuinielaFillerKeras(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False,
-                 loss='categorical_crossentropy',
-                 optimizer="adam",
-                 metrics=['accuracy'],
-                 dropout=True):
+    def __init__(self, liga=1, update_data=False, **kwargs):
         super(QuinielaFillerKeras, self).__init__(liga=liga, update_data=update_data)
-        self.model = self.create_model(loss=loss, optimizer=optimizer, metrics=metrics, dropout=dropout)
+        self.model = self.create_model(**kwargs)
 
     def create_model(self,
                      loss='categorical_crossentropy',
-                     optimizer='adam',
+                     optimizer=keras.optimizers.Adam(learning_rate=0.001),
                      metrics=['accuracy'],
                      dropout=True):
         model = Sequential()
-        model.add(Dense(16, activation='relu', input_shape=(self.X.shape[1],)))
+        model.add(Dense(16, activation=None, input_shape=(self.X.shape[1],)))
         if dropout:
-            model.add(Dropout(.1))
+            model.add(Dropout(.5))
         model.add(Dense(8, activation='relu'))
         if dropout:
-            model.add(Dropout(.1))
+            model.add(Dropout(.5))
         model.add(Dense(3, activation='softmax'))
 
         model.compile(loss=loss,
@@ -207,9 +216,9 @@ class QuinielaFillerKeras(QuinielaFillerBase):
 
     # Trains model_nn from historic_data inputted in initialization
     # + Predictions + Past odds
-    def train(self, epochs=200, batch_size=5, verbose=1, early_stopping=True, callbacks=[]):
+    def train(self, epochs=50, batch_size=16, verbose=1, early_stopping=True, callbacks=[]):
         if early_stopping:
-            callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+            callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=3, min_delta=0.01)
             callbacks.append(callback)
         return self.model.fit(self.X, self.Y, epochs=epochs, batch_size=batch_size, verbose=verbose,
                               callbacks=callbacks)
@@ -222,7 +231,8 @@ class QuinielaFillerKeras(QuinielaFillerBase):
         df_num[["AvgH", "AvgA", "AvgD"]] = 1 / df_num[
             ["full_time_result.1", "full_time_result.2", "full_time_result.X"]]
         df_num.drop(["full_time_result.1", "full_time_result.2", "full_time_result.X"], axis=1, inplace=True)
-        xt_scaled = self.rob_scaler.transform(df_num)
+        xt_scaled = self.rob_scaler.transform(df_num[self.columns])  # Apply transformer + Resort columns
+
         return self.model.predict(xt_scaled, batch_size=batch_size, verbose=1)
 
     def visualize_model(self):
@@ -240,9 +250,9 @@ class QuinielaFillerKeras(QuinielaFillerBase):
 
 
 class QuinielaFillerRF(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False, *args):
+    def __init__(self, liga=1, update_data=False, **kwargs):
         super(QuinielaFillerRF, self).__init__(liga=liga, update_data=update_data)
-        self.model = self.create_model(*args)
+        self.model = self.create_model(**kwargs)
 
     def create_model(self, criterion='entropy', n_estimators=100, max_depth=16, warm_start=True, verbose=1):
         model = RandomForestClassifier(criterion=criterion, n_estimators=n_estimators, max_depth=max_depth,
@@ -255,28 +265,14 @@ class QuinielaFillerRF(QuinielaFillerBase):
         self.Y = self.raw_data["FTR"]
         self.model.fit(self.X, self.Y)
 
-    def predict(self, xt):
-        categorical = ["home_team", "away_team", "time"]
-        unnecessary = ["total_valA", "total_valH"]
-
-        df_num = xt.drop(columns=categorical + unnecessary)
-        df_num[["AvgH", "AvgA", "AvgD"]] = 1 / df_num[
-            ["full_time_result.1", "full_time_result.2", "full_time_result.X"]]
-        df_num.drop(["full_time_result.1", "full_time_result.2", "full_time_result.X"], axis=1, inplace=True)
-        xt_scaled = self.rob_scaler.transform(df_num)
-
-        preds = self.model.predict_proba(xt_scaled)
-        return preds
-
     def evaluate(self, xt, yt):
         return self.model.evaluate(xt, yt, verbose=1)
 
 
-
 class QuinielaFillerKNN(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False, n_neighbors=5):
+    def __init__(self, liga=1, update_data=False, **kwargs):
         super(QuinielaFillerKNN, self).__init__(liga=liga, update_data=update_data)
-        self.model = self.create_model(n_neighbors=n_neighbors)
+        self.model = self.create_model(**kwargs)
 
     def create_model(self, n_neighbors):
         return KNeighborsClassifier(n_neighbors=n_neighbors)
@@ -285,63 +281,73 @@ class QuinielaFillerKNN(QuinielaFillerBase):
         self.Y = self.raw_data["FTR"]
         self.model.fit(self.X, self.Y)
 
-    def predict(self, xt):
-        categorical = ["home_team", "away_team", "time"]
-        unnecessary = ["total_valA", "total_valH"]
+    def evaluate(self, xt, yt):
+        return self.model.evaluate(xt, yt, verbose=1)
 
-        df_num = xt.drop(columns=categorical + unnecessary)
-        df_num[["AvgH", "AvgA", "AvgD"]] = 1 / df_num[
-            ["full_time_result.1", "full_time_result.2", "full_time_result.X"]]
-        df_num.drop(["full_time_result.1", "full_time_result.2", "full_time_result.X"], axis=1, inplace=True)
-        xt_scaled = self.rob_scaler.transform(df_num)
 
-        preds = self.model.predict_proba(xt_scaled)
-        return preds
+class QuinielaFillerQDA(QuinielaFillerBase):
+    def __init__(self, liga=1, update_data=False, **kwargs):
+        super(QuinielaFillerQDA, self).__init__(liga=liga, update_data=update_data)
+        self.model = self.create_model(**kwargs)
+
+    def create_model(self, priors=None, reg_param=0.0):
+        return QDA(priors=priors, reg_param=reg_param)
+
+    def train(self):
+        self.Y = self.raw_data["FTR"]
+        self.model.fit(self.X, self.Y)
 
     def evaluate(self, xt, yt):
         return self.model.evaluate(xt, yt, verbose=1)
 
 
-# TODO: QDA
-class QuinielaFillerQDA(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False):
-        super(QuinielaFillerQDA, self).__init__(liga=liga, update_data=update_data)
-
-    def create_model(self):
-        pass
-
-    def train(self):
-        pass
-
-    def predict(self, xt):
-        pass
-
-
-# TODO: LSTM
-class QuinielaFillerLSTM(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False):
-        super(QuinielaFillerLSTM, self).__init__(liga=liga, update_data=update_data)
-
-    def create_model(self):
-        pass
-
-    def train(self):
-        pass
-
-    def predict(self, xt):
-        pass
-
-
-# TODO: SVM
 class QuinielaFillerSVM(QuinielaFillerBase):
-    def __init__(self, liga=1, update_data=False):
+    def __init__(self, liga=1, update_data=False, **kwargs):
         super(QuinielaFillerSVM, self).__init__(liga=liga, update_data=update_data)
+        self.model = self.create_model(**kwargs)
 
-    def create_model(self):
-        pass
+    def create_model(self, C=1.0, kernel="rbf"):
+        return SVC(C=C, kernel=kernel, probability=True)
 
     def train(self):
-        pass
+        self.Y = self.raw_data["FTR"]
+        self.model.fit(self.X, self.Y)
 
-    def predict(self, xt):
-        pass
+    def evaluate(self, xt, yt):
+        return self.model.evaluate(xt, yt, verbose=1)
+
+
+class QuinielaFillerLSTM(QuinielaFillerKeras):
+    def __init__(self, liga=1, update_data=False, **kwargs):
+        super(QuinielaFillerLSTM, self).__init__(liga=liga, update_data=update_data)
+        self.model = self.create_model(**kwargs)
+
+    def create_model(self,
+                     loss='categorical_crossentropy',
+                     optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                     metrics=['accuracy'],
+                     dropout=True):
+        model = Sequential()
+        model.add(SpatialDropout1D(0.1, input_shape=(16, 1)))
+        model.add(LSTM(16, dropout=0.2, recurrent_dropout=0.2))
+        model.add(Dense(3, activation='softmax'))
+
+        model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        return model
+
+
+class QuinielaFillerGBC(QuinielaFillerBase):
+    def __init__(self, liga=1, update_data=False, **kwargs):
+        super(QuinielaFillerGBC, self).__init__(liga=liga, update_data=update_data)
+        self.model = self.create_model(**kwargs)
+
+    def create_model(self, learning_rate=0.1, n_estimators=100, verbose=1):
+        model = GradientBoostingClassifier(learning_rate=learning_rate, n_estimators=n_estimators, verbose=verbose)
+        return model
+
+    def train(self):
+        self.Y = self.raw_data["FTR"]
+        self.model.fit(self.X, self.Y)
+
+    def evaluate(self, xt, yt):
+        return self.model.evaluate(xt, yt, verbose=1)
